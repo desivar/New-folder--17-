6b2ea16ac7b84371de75e68ca146231b1bb01ebd
desivar <jillian_desire@yahoo.es>
2025-05-28 15:45:47 -0600

    Change typo in db.js

--- icecream-api/config/db.js
+++ icecream-api/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB Connected successfully!');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
